82d40286784a2335d342a21b2411be3e121b252d
aayushi1425 <aayushimaigale@gmail.com>
2026-02-02 12:53:28 +0530

    feat:frontend folder

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import AuthPage from "./pages/AuthPage";
+import Dashboard from "./pages/Dashboard";
+
 export default function App() {
-    return (
-        <div>
-            <h1>hello</h1>
-        </div>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<AuthPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="*" element={<AuthPage />} />
+
+    </Routes>
+  );
 }
---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#25343F",
+        light: "#EAEFEF",
+        muted: "#BFC9D1",
+        accent: "#FF9B51",
+      },
+    },
+  },
+  plugins: [],
+};
